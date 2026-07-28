ea79d4ae6786956f5c18fd2b6784c857fb8bf3fd
Mohammadreza <mohammad.za9297@gmail.com>
2026-07-28 17:13:17 +0330

    User Login form is added

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,19 +1,24 @@
 from flask import Blueprint,render_template,request,redirect,flash,url_for
 from models.tables import User
 from extentions import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,logout_user
 import re
+
 
 
 bp = Blueprint("user", __name__, url_prefix="/user")
 
-
-
+#===================
+# USER Dashboard
+#===================
 @bp.route("/dashboard", methods=["GET"])
 def dashboard ():
     return render_template("user/user_dashboard.html")
 
-
+#===================
+# USER Register
+#===================
 @bp.route("/register", methods=["GET", "POST"])
 def register():
 
@@ -105,3 +110,45 @@ def register():
 
     flash("ثبت نام با موفقیت انجام شد.", "success")
     return redirect(url_for("user.dashboard"))
+
+
+#===================
+# USER Login
+#===================
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        phone = request.form.get("phone").strip()
+        password = request.form.get("password")
+
+        user = User.query.filter_by(phone=phone).first()
+
+        if user is None:
+            flash("شماره موبایل وارد شده یافت نشد.", "error")
+            return redirect(url_for("user.login"))
+
+        if not check_password_hash(user.password, password):
+            flash("رمز عبور اشتباه است.", "danger")
+            return redirect(url_for("user.login"))
+
+        login_user(user)
+
+        flash("با موفقیت وارد حساب کاربری شدید.", "success")
+
+        return redirect(url_for("user.dashboard"))
+
+    return render_template("user/user_login.html")
+
+
+#===================
+# USER Logout
+#===================
+@bp.route("/logout")
+def logout():
+
+    logout_user()
+
+    flash("از حساب کاربری خارج شدید.", "success")
+    return redirect(url_for("general.home"))
