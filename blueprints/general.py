069b30c80cba07d0d4c127c33515c9345c6b62cf
Mohammadreza <mohammad.za9297@gmail.com>
2026-08-09 00:12:53 +0330

    all tags link in header is updates

--- blueprints/general.py
+++ blueprints/general.py
@@ -10,3 +10,26 @@ def home ():
 
 
 
+@bp.route("/general/about")
+def about ():
+    return render_template("about.html")
+
+@bp.route("/general/bakhsh_ha")
+def bakhsh_ha ():
+    return render_template("bakhsh_ha.html")
+
+@bp.route("/general/clinic_ha")
+def clinic_ha ():
+    return render_template("clinic_ha.html")
+
+@bp.route("/general/akhbar")
+def akhbar ():
+    return render_template("akhbar.html")
+
+@bp.route("/general/gallery")
+def gallery ():
+    return render_template("gallery.html")
+
+@bp.route("/general/ertebat_ba_ma")
+def ertebat_ba_ma ():
+    return render_template("ertebat_ba_ma.html")
